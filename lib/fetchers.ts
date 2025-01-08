4144fbd21fb3cb087c1033011aae08bb9cfb59dc
Wilson Kinyua <wilsonkinyuam@gmail.com>
2025-01-08 14:47:17 +0300

    feat: add fetcher function to retrieve all gigs from the API

---
+++ lib/fetchers.ts
@@ -0,0 +1,14 @@
+'use server';
+import axios from 'axios';
+
+// Fetch all gigs
+export async function getAllGigs() {
+  try {
+    const response = await axios.get(
+      'https://joinstartup.africa/api/v1/gigs?with=tags,user,mediafiles,faqs,categories,gigpricingplans,gigaddons'
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch gigs. Please try again later.');
+  }
+}
