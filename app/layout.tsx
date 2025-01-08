ee5f24710a6a369bc26091c32a72ec35c1e21871
Wilson Kinyua <wilsonkinyuam@gmail.com>
2025-01-08 16:05:56 +0300

    feat: update app title to 'Gig Economy' and adjust Navbar styles for improved layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Gig",
+  title: "Gig Economy",
   description: "A simple app to find gigs",
 };
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,12 +6,12 @@ import Image from 'next/image';
 
 export default function Navbar() {
     return (
-        <nav className="p-6 uppercase shadow-md text-primary font-">
+        <nav className="p-5 uppercase shadow-md text-primary font-medium">
             <div className="container mx-auto flex justify-between items-center">
                 <div>
-                    <Image src="https://www.gigeconomydata.org/sites/default/files/styles/full_content/public/2018-05/gigeconomydata.org__0.png" alt="Gigs Economy" width={100} height={50} className='w-full lg:h-20 h-14' />
+                    <Image src="https://www.gigeconomydata.org/sites/default/files/styles/full_content/public/2018-05/gigeconomydata.org__0.png" alt="Gigs Economy" width={100} height={50} className='w-full h-14' />
                 </div>
-                <div className="hidden md:flex space-x-8 text-lg items-center">
+                <div className="hidden md:flex space-x-10 text-lg items-center">
                     <Link href="#">Home</Link>
                     <Link href="#">About Us</Link>
                     <Link href="#">Services</Link>
