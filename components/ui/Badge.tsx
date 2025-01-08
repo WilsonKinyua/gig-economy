266af6b2a22d0edc794aa41b40af58ed4dace85d
Wilson Kinyua <wilsonkinyuam@gmail.com>
2025-01-08 16:12:30 +0300

    feat: change Badge component from span to div for improved styling flexibility

--- components/ui/Badge.tsx
+++ components/ui/Badge.tsx
@@ -2,8 +2,8 @@ export default function Badge({ children }: {
     children: React.ReactNode;
 }) {
     return (
-        <span className="bg-primary text-white text-xs font-semibold uppercase rounded-full px-3 py-2">
+        <div className="bg-primary text-white text-xs font-semibold uppercase rounded-full px-3 py-2">
             {children}
-        </span>
+        </div>
     );
 }
